9710bcbf9c1f7194b828f9cafd61406de72262a7
Krito Lenzi <k77rito@gmail.com>
2021-03-08 09:00:30 -0300

    admin

--- Mascotas/apps/transito/admin.py
+++ Mascotas/apps/transito/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.base_datos.models import Usuario, MascotaTransito, MascotasAdopcion, Adopcion, Denuncias, Transito, Veterinarias
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(MascotasAdopcion)
+admin.site.register(MascotaTransito)
+admin.site.register(Adopcion)
+admin.site.register(Denuncias)
+admin.site.register(Transito)
+admin.site.register(Veterinarias)
+
